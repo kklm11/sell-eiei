e46f6e90299ea04fe8cd1609c7bf65346e5580a4
natachx <sunda8591@gmail.com>
2026-07-01 21:44:13 +0700

    Update particles.js

--- js/particles.js
+++ js/particles.js
@@ -1,1 +1,49 @@
-
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 80
+    },
+    color: {
+      value: "#a855f7"
+    },
+    shape: {
+      type: "circle"
+    },
+    opacity: {
+      value: 0.4
+    },
+    size: {
+      value: 3
+    },
+    move: {
+      enable: true,
+      speed: 1
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#8b5cf6",
+      opacity: 0.2,
+      width: 1
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "grab"
+      },
+      resize: true
+    },
+    modes: {
+      grab: {
+        distance: 180,
+        line_linked: {
+          opacity: 0.6
+        }
+      }
+    }
+  },
+  retina_detect: true
+});
